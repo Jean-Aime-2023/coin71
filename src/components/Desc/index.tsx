226b4a386e9a3ae783aae7b17d88ef7089437b01
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2023-08-16 12:31:33 +0200

    overflow

--- src/components/Desc/index.tsx
+++ src/components/Desc/index.tsx
@@ -3,7 +3,7 @@ import Side2 from '../Side2'
 import Side3 from '../Side3'
 const index = () => {
   return (
-    <div className='bg-gray-400 p-12 grid xl:grid-cols-3 gap-10 lg:grid-cols-2 max-lg:px-[7rem] max-sm:px-3'>
+    <div className='bg-gray-400 p-12 grid xl:grid-cols-3 gap-10 lg:grid-cols-2 max-lg:px-[7rem] max-sm:px-3 overflow-x-hidden'>
         {/* <Side1/>
         <Side2/>
         <Side3/> */}
