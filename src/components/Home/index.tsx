01f1f38155314717ac240bc6191d19e6648c0e55
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2023-08-16 18:31:11 +0200

    final commit

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -25,7 +25,6 @@ const index: React.FC = () => {
   const [menu, setMenu] = useState(false);
 
   const toggleMenu = () => {
-    console.log("clicked");
     setMenu(!menu);
   };
 
@@ -33,6 +32,10 @@ const index: React.FC = () => {
 
   const toggleCloseMenu = () => {
     setCloseMenu(true);
+  };
+
+  const toggleCloseMenu1 = () => {
+    setMenu(false);
   };
 
   return (
@@ -71,73 +74,72 @@ const index: React.FC = () => {
             </li>
           </ul>
 
-          <div className="max-menu:flex menu:hidden" onClick={toggleMenu}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-list"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fillRule="evenodd"
-                d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-              />
-            </svg>
-          </div>
+      <div className="max-menu:flex menu:hidden" onClick={toggleMenu}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-list"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fillRule="evenodd"
+            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+          />
+        </svg>
+      </div>
 
-          <div className={!menu?"":"flex"} id="container">
-          <div
-            id="container"
-            className={!closeMenu?"":"fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center z-50"}
-          >
-            <div className={!closeMenu?"hidden":"block"}>
-            <ul
-              className= "absolute top-5 left-[50%] bg-gray-100 p-4 rounded-2xl flex flex-col gap-3"
-              
-            >
+      <div className={menu ? '' : 'flex'}>
+        <div
+          className={menu ? 'fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center z-50' : 'hidden'}
+        >
+          <div className={menu ? 'block' : 'hidden'}>
+            <ul className="absolute top-5 left-[50%] bg-gray-100 p-4 rounded-2xl flex flex-col gap-3">
               <li>
                 <a href="/" className="hover:text-red-500">
                   Home
                 </a>
               </li>
               <li>
-                <a href="#" className="hover:text-red-500">
-                  Shop
-                </a>
-              </li>
+              <a href="#" className="hover:text-red-500">
+                Shop
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-red-500">
+                Coupons
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-red-500">
+                Apps
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-red-500">
+                My Favorite
+              </a>
+            </li>
+              {/* Other menu items */}
               <li>
-                <a href="#" className="hover:text-red-500">
-                  Coupons
-                </a>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-x-circle ml-8"
+                  viewBox="0 0 16 16"
+                  onClick={toggleCloseMenu1}
+                >
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                </svg>
               </li>
-              <li>
-                <a href="#" className="hover:text-red-500">
-                  Apps
-                </a>
-              </li>
-              <li className="pb-4">
-                <a href="#" className="hover:text-red-500">
-                  My Favorite
-                </a>
-              </li>
-              <li>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-x-circle ml-8"
-              viewBox="0 0 16 16"
-              onClick={toggleCloseMenu}
-            >
-              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-            </svg></li>
-            </ul>  
-            </div>
-          </div></div>
+            </ul>
+          </div>
+        </div>
+      </div>
 
 
           <div className="cursor-pointer">
